a34afb28d4cfec81df9f8598ac359d5c59772aec
Rui <rui@movio.la>
2024-06-17 22:14:51 -0700

    chore: make jitter buffer as an argument

--- apis/StreamingAvatarApi.ts
+++ apis/StreamingAvatarApi.ts
@@ -401,14 +401,17 @@ export class StreamingAvatarApi extends runtime.BaseAPI {
 
             await this.startStreamingAvatar({startSessionRequest: {sdp: localDescription, sessionId: this.sessionId}});
 
-            let receivers = this.peerConnection.getReceivers();
-            
-            
-            receivers.forEach(receiver => {
-                if('jitterBufferTarget' in receiver){
-                    receiver.jitterBufferTarget = 500;
-                }
-            });
+            // Set jitter buffer
+            if (this.configuration.jitterBuffer !== undefined){
+                let receivers = this.peerConnection.getReceivers();
+                
+                
+                receivers.forEach(receiver => {
+                    if('jitterBufferTarget' in receiver){
+                        receiver.jitterBufferTarget = this.configuration.jitterBuffer;
+                    }
+                });
+            }
 
             debug.print("Session started successfully");
 
